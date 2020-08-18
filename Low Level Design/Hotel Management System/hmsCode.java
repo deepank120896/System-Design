f2e33df06a2a0695596311002dfd007125588e82
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-08-19 00:15:44 +0530

    Admin and Guest Objects

--- Low Level Design/Hotel Management System/hmsCode.java
+++ Low Level Design/Hotel Management System/hmsCode.java
@@ -14,7 +14,7 @@ class Location {
 }
 
 class Room {
-	String roomNumber;
+	String roomId;
 	RoomStyle roomStyle;
 	RoomStatus roomStatus;
 	Double bookingPrice;
@@ -49,3 +49,87 @@ class HouseKeepingLogs {
 	
 	public void addRoom(Room room);
 }
+
+abstract class Person {
+	String name;
+	Account accountDetail;
+	String phone;
+}
+
+public class Account {
+	String username;
+	String password;
+	AccountStatus accountStatus;
+}
+
+public enum AccountStatus {
+	ACTIVE, CLOSED, BLOCKED;
+}
+
+class HouseKeeper extends Person {
+	public List<Room> getRoomsServiced(Date date);
+}
+
+class Guest extends Person {
+	Search searchObj;
+	Booking bookingObj;
+	
+	public void checkInGuest(Guest guest, RoomBooking bookingInfo);
+	public void checkOutGuest(Guest guest, RoomBooking bookingInfo);
+}
+
+class Admin extends Person {
+	public void addRoom(RoomDetail roomDetail);
+	public void deleteRoom(String roomId);
+	public void editRoom(RoomDetail roomDetail);
+}
+
+class Search {
+	public List<Room> searchRoom(RoomStyle roomStyle, Date startDate, int duration);
+}
+
+class Booking {
+	public RoomBooking createBooking(Guest guestInfo);
+	public RoomBooking cancelBooking(int bookingId);
+}
+
+class RoomBooking {
+	String bookingId;
+	Date startDate;
+	Int durationInDays;
+	BookingStatus bookingStatus;
+	List<Guest> guestList;
+	List<Room> roomInfo;
+	BaseRoomCharge totalRoomCharges;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
